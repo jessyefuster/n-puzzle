7919be6df1164d1a7a3c487e488c09054c5ea600
Jessye <jfuster@student.42.fr>
2017-06-20 13:58:11 +0200

    Heuristics implementation

--- srcs/npuzzle.py
+++ srcs/npuzzle.py
@@ -8,9 +8,9 @@ class Puzzle:
 		self.size = len(puzzle)
 		self.tsize = pow(self.size, 2)
 
-		self.goal = list(range(1, self.tsize))
-		self.goal.append(0)
+		self.goal = self.makeGoal()
 
+		self.heuristic = self.manhattan
 		self.swaps = [self.swapUp, self.swapDown, self.swapLeft, self.swapRight]
 
 
@@ -20,6 +20,18 @@ class Puzzle:
 			for cell in line:
 				print(cell, end=' ')
 			print()
+
+	def makeGoal(self):
+		puzzle = [ [0] * self.size for i in range(0, self.size) ]
+		number = 1
+
+		for y, line in enumerate(puzzle):
+			for x, cell in enumerate(line):
+				puzzle[y][x] = 0 if ( y == self.size - 1 and x == self.size - 1 ) else number
+
+				number += 1
+
+		return (puzzle)
 
 	def findEmptyCell(self):
 		for y, line in enumerate(self.puzzle):
@@ -51,13 +63,57 @@ class Puzzle:
 			return (True)
 		else: #odd
 			return (False)
+
+	def searchPosition(self, value):
+		for y, line in enumerate(self.goal):
+			for x, number in enumerate(line):
+				if value == number:
+					return (x, y)
 	#################
 
+
+	### HEURISTIC ###
+	def	manhattan(self, puzzle):
+		'''
+		dist = (start_x - end_x) + (start_y - end_y)
+		'''
+		dist = 0
+
+		for start_y, line in enumerate(puzzle):
+			for start_x, value in enumerate(line):
+				if value:
+					end_x, end_y = self.searchPosition(value)
+					dist += abs(start_x - start_y) + abs(end_x - end_y)
+		return (dist)
+
+	def	euclidian(self, puzzle):
+		'''
+		dist = sqrt( (ax - bx)**2 + (ay - by)**2 )
+		'''
+		dist = 0
+
+		for start_y, line in enumerate(puzzle):
+			for start_x, value in enumerate(line):
+				if value:
+					end_x, end_y = self.searchPosition(value)
+					dist += ( (start_x - start_y) ** 2 + (start_x - start_y) ** 2 ) ** 0.5
+		return (dist)
+
+	def	misplaced(self, puzzle):
+		dist = 0
+
+		for y, line in enumerate(puzzle):
+			for x, value in enumerate(line):
+				if value:
+					if (x, y) != self.searchPosition(value):
+						dist += 1
+		return (dist)
+	#################
 
 	###   SWAPS   ###
 	def swapUp(self, puzzle, empty):
 		y, x = empty
-		if y - 1 > 0:
+		if y - 1 >= 0:
 			puzzle[y][x], puzzle[y - 1][x] = puzzle[y - 1][x], puzzle[y][x]
 			return (puzzle)
 		return (False)
@@ -69,7 +125,7 @@ class Puzzle:
 		return (False)
 	def swapLeft(self, puzzle, empty):
 		y, x = empty
-		if x - 1 > 0:
+		if x - 1 >= 0:
 			puzzle[y][x], puzzle[y][x - 1] = puzzle[y][x - 1], puzzle[y][x]
 			return (puzzle)
 		return (False)
@@ -92,12 +148,33 @@ class Puzzle:
 			puzzle = swap(puzzle, empty)
 			
 			if puzzle:
-				self.printPuzzle(puzzle)
-				print()
 				nextStates.append(puzzle)
+
+		return (nextStates)
 
 	def nextStep(self):
 		nextArray = self.nextStates()
+		costArray = []
+		for state in nextArray:
+			costArray.append(self.heuristic(state))
+
+		# self.printPuzzle(self.puzzle)
+		# print(costArray)
+		# print()
+		# for state in nextArray:
+		# 	self.printPuzzle(state)
+		# 	print()
+
+		# print("\n==============")
+		# print()
+
+		leastCost = min(costArray)
+		if costArray.count(leastCost) > 1:
+			pass
+
+		optimalState = nextArray[costArray.index(min(costArray))]
+
+		return (optimalState)
 	#################
 
 
@@ -108,8 +185,13 @@ class Puzzle:
 
 
 	def resolve(self):
-		if not self.resolved():
+		i = 0
+
+		while not self.resolved() and i < 5:
 			self.puzzle = self.nextStep()
+			# self.printPuzzle(self.puzzle)
+			# print()
+			i += 1
 	#################
 		
 
@@ -120,17 +202,17 @@ class Puzzle:
 if __name__ == '__main__':
 
 	# solvable
-	puzzle = [	[5, 12, 0, 13],
-				[15, 8, 7, 6],
+	puzzle = [	[5, 12, 7, 13],
+				[15, 8, 0, 6],
 				[14, 4, 3, 2],
 				[9, 1, 11, 10]	]
 
 
 	P = Puzzle(puzzle)
-	P.printPuzzle(P.puzzle)
-	print('\n=============')
 
-	# if not P.solvable():
-	# 	print("Not solvable")
-	# else:
-	P.resolve()
+
+
+	if not P.solvable():
+		print("Not solvable")
+	else:
+		P.resolve()
